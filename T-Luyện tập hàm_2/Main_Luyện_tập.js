3250d0b885358c7612cddffc8224e5310e6d56c2
duyduong65 <tg.bluesky66@gmail.com>
2019-09-11 22:07:31 +0700

    add ex_2,3

--- T-Luyện tập hàm_2/Main_Luyện_tập.js
+++ T-Luyện tập hàm_2/Main_Luyện_tập.js
@@ -1,6 +1,22 @@
 function ex_1() {
     let inputNumber = Number(prompt("Nhập vào số cần tính: "));
     let square = inputNumber * inputNumber;
-    alert("Bình phương của "+inputNumber+ " là: "+ square);
+    alert("Bình phương của " + inputNumber + " là: " + square);
+}
 
+function acreage() {
+    let radius = Number(prompt("Nhập vào bán kính: "));
+    let perimeter = Math.PI * radius * 2;
+    let acreage = Math.PI * radius * radius;
+    alert("Chu vi hình tròn là: " + perimeter);
+    alert("Diện tích hình tròn là: " + acreage);
+}
+
+function factorial() {
+    let inputNumber = Number(prompt("Nhập vào số cần tính: "));
+    let factorial = 1;
+    for (let i = inputNumber; i > 0; i--) {
+        factorial = factorial * i;
+    }
+    alert("giai thừa của " + inputNumber + " là: " + factorial);
 }
