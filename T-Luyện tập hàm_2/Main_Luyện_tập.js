656128a15abfbeeca2736befb4a7240a33ba6b7f
duyduong65 <tg.bluesky66@gmail.com>
2019-09-11 21:21:09 +0700

    add ex_1

---
+++ T-Luyện tập hàm_2/Main_Luyện_tập.js
@@ -0,0 +1,6 @@
+function ex_1() {
+    let inputNumber = Number(prompt("Nhập vào số cần tính: "));
+    let square = inputNumber * inputNumber;
+    alert("Bình phương của "+inputNumber+ " là: "+ square);
+
+}
