2ba6654728e9c46e1f83e538e7a2d478f3204b14
duyduong65 <tg.bluesky66@gmail.com>
2019-09-11 15:25:16 +0700

    add_L-ConvertToMeter

---
+++ L-ConverFeetToMeter/Main_toMeter.js
@@ -0,0 +1,10 @@
+
+function convertToMet(foot) {
+    let met = foot * 0.305;
+    document.getElementById('value_1').innerHTML = met;
+}
+
+function convertToFoot(met) {
+    let foot = met * 3.279;
+    document.getElementById('value_2').innerHTML = foot;
+}
