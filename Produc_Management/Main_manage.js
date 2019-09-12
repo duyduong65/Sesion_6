0133c45b1dec0601c68cca9b01d8bec1c9d435a0
duyduong65 <tg.bluesky66@gmail.com>
2019-09-12 16:16:33 +0700

    đã edit được

--- Produc_Management/Main_manage.js
+++ Produc_Management/Main_manage.js
@@ -1,24 +1,73 @@
-let arr = ["Sony Xperia"];
+let arr = ["Sony Xperia", "Samsung Galaxy", "Nokia 6", "Xiaomi Redmi Note 4", "Apple iphone 6s", "Xiaomi 5s Plus", "Apple Iphone 8 Plus", "Fujitsu F-04E", "Oppo A71"];
 let element = document.getElementById('inputString');
+let dem = 0;
 for (let i = 0; i < arr.length; i++) {
-    document.getElementById("elementAraay").innerHTML += "<h4>" + arr[i] + "</h4>" + " <br> ";
-    document.getElementById('edit_1').innerHTML = "<button><h4>edit</h4></button>" ;
-    document.getElementById('delete').innerHTML = " <button><h4>Delete</h4></button>"
+    document.getElementsByClassName("element")[dem].innerHTML += arr[i] + +" <br> ";
+    document.getElementsByClassName('edit')[dem].innerHTML = "<button><h4>edit</h4></button>";
+    document.getElementsByClassName('delete')[dem].innerHTML = " <button><h4>Delete</h4></button>"
+    dem++
 }
-let dem=0;
+
 function addElement() {
     arr.push(element.value);
-    dem++
-    document.getElementById('element').innerHTML += "<h4>" + arr[arr.length - 1] + "<br>" + "</h4>";
+    document.getElementsByClassName('element')[dem].innerHTML += "<h4>" + arr[arr.length - 1] + "<br>" + "</h4>";
     document.getElementById('inputString').value = "";
     document.getElementById('inputString').focus();
     document.getElementsByClassName('edit')[dem].innerHTML = "<button><h4>edit</h4></button>";
-    document.getElementsByClassName('delete')[dem].innerHTML = "<button><h4>delete</h4></button>"+"<br>";
+    document.getElementsByClassName('delete')[dem].innerHTML = "<button><h4>delete</h4></button>" + "<br>";
+    dem++
+}
+
+function editProduct(val) {
+    switch (val) {
+        case 0 :
+            document.getElementById('inputString').value = arr[val];
+            break;
+        case 1 :
+            document.getElementById('inputString').value = arr[val];
+            break;
+        case 2 :
+            document.getElementById('inputString').value = arr[val];
+            break;
+        case 3 :
+            document.getElementById('inputString').value = arr[val];
+            break;
+        case 4 :
+            document.getElementById('inputString').value = arr[val];
+            break;
+        case 5 :
+            document.getElementById('inputString').value = arr[val];
+            break;
+        case 6 :
+            document.getElementById('inputString').value = arr[val];
+            break;
+        case 7 :
+            document.getElementById('inputString').value = arr[val];
+            break;
+        case 8 :
+            document.getElementById('inputString').value = arr[val];
+            break;
+        case 9 :
+            document.getElementById('inputString').value = arr[val];
+            break;
+        case 10 :
+            document.getElementById('inputString').value = arr[val];
+            break;
+        case 11 :
+            document.getElementById('inputString').value = arr[val];
+            break;
+        case 12 :
+            document.getElementById('inputString').value = arr[val];
+            break;
+        case 13 :
+            document.getElementById('inputString').value = arr[val];
+            break;
 
 
-
+    }
 
 }
 
-// document.getElementById('delete').innerHTML = " <button>edit</button>";
-// , "Samsung Galaxy", "Nokia 6", "Xiaomi Redmi Note 4", "Apple iphone 6s", "Xiaomi 5s Plus", "Apple Iphone 8 Plus", "Fujitsu F-04E", "Oppo A71"
+function deleteProduct() {
+
+}
