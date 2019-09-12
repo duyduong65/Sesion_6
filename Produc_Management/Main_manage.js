f79440abb84e026d7717d1edab4468f364427dee
duyduong65 <tg.bluesky66@gmail.com>
2019-09-12 16:53:06 +0700

    final function edit

--- Produc_Management/Main_manage.js
+++ Produc_Management/Main_manage.js
@@ -2,7 +2,7 @@ let arr = ["Sony Xperia", "Samsung Galaxy", "Nokia 6", "Xiaomi Redmi Note 4", "A
 let element = document.getElementById('inputString');
 let dem = 0;
 for (let i = 0; i < arr.length; i++) {
-    document.getElementsByClassName("element")[dem].innerHTML += arr[i] + +" <br> ";
+    document.getElementsByClassName("element")[dem].innerHTML = arr[i] +" <br> ";
     document.getElementsByClassName('edit')[dem].innerHTML = "<button><h4>edit</h4></button>";
     document.getElementsByClassName('delete')[dem].innerHTML = " <button><h4>Delete</h4></button>"
     dem++
@@ -10,7 +10,7 @@ for (let i = 0; i < arr.length; i++) {
 
 function addElement() {
     arr.push(element.value);
-    document.getElementsByClassName('element')[dem].innerHTML += "<h4>" + arr[arr.length - 1] + "<br>" + "</h4>";
+    document.getElementsByClassName('element')[dem].innerHTML += arr[arr.length - 1] + "<br>";
     document.getElementById('inputString').value = "";
     document.getElementById('inputString').focus();
     document.getElementsByClassName('edit')[dem].innerHTML = "<button><h4>edit</h4></button>";
@@ -18,52 +18,65 @@ function addElement() {
     dem++
 }
 
+let str = document.getElementById('inputString');
+
+function fix() {
+    for (let i = 0; i < arr.length; i++) {
+        if (str.value == arr[i]) {
+            let conVertProduct = prompt("Nhập vào tên muốn đổi: ");
+            arr[i] = conVertProduct;
+            document.getElementsByClassName('element')[i].innerHTML = arr[i];
+            document.getElementById('inputString').value = "";
+            document.getElementById('inputString').focus();
+
+        }
+    }
+}
+
 function editProduct(val) {
     switch (val) {
         case 0 :
-            document.getElementById('inputString').value = arr[val];
+            str.value = arr[val];
             break;
         case 1 :
-            document.getElementById('inputString').value = arr[val];
+            str = arr[val];
             break;
         case 2 :
-            document.getElementById('inputString').value = arr[val];
+            str.value = arr[val];
             break;
         case 3 :
-            document.getElementById('inputString').value = arr[val];
+            str.value = arr[val];
             break;
         case 4 :
-            document.getElementById('inputString').value = arr[val];
+            str.value = arr[val];
             break;
         case 5 :
-            document.getElementById('inputString').value = arr[val];
+            str.value = arr[val];
             break;
         case 6 :
-            document.getElementById('inputString').value = arr[val];
+            str.value = arr[val];
             break;
         case 7 :
-            document.getElementById('inputString').value = arr[val];
+            str.value = arr[val];
             break;
         case 8 :
-            document.getElementById('inputString').value = arr[val];
+            str.value = arr[val];
             break;
         case 9 :
-            document.getElementById('inputString').value = arr[val];
+            str.value = arr[val];
             break;
         case 10 :
-            document.getElementById('inputString').value = arr[val];
+            str.value = arr[val];
             break;
         case 11 :
-            document.getElementById('inputString').value = arr[val];
+            str.value = arr[val];
             break;
         case 12 :
-            document.getElementById('inputString').value = arr[val];
+            str.value = arr[val];
             break;
         case 13 :
-            document.getElementById('inputString').value = arr[val];
+            str.value = arr[val];
             break;
-
-
     }
 
 }
